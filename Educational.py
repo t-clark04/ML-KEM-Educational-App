3293695d88018b438268699232937f4dc70dcbde
t-clark04 <tclark23+UND@nd.edu>
2026-01-23 12:01:42 -0500

    Update Educational.py

--- Educational.py
+++ Educational.py
@@ -268,8 +268,8 @@ st.set_page_config(layout="wide", page_title="ML-KEM KeyGen Path")
 st.title("Understanding the ML-KEM Cryptosystem")
 st.markdown("In August 2024, the National Institute of Standards and Technology (NIST) announced the approval of three new encryption standards following an eight year process of submission and evaluation, with the **ML-KEM** cryptosystem becoming the standard for general encryption.")
 st.markdown("**Why was it needed?**")
-st.markdown("- Because the current encryption schemes in place around the globe were built to withstand attacks from *classical* computers, but the rise of *quantum* computers puts the security of these cryptosystems at severe risk of being broken.")
-st.markdown("- The ML-KEM was designed specifically with a post-quantum world in mind. It will soon be used by government agencies and private companies alike to keep information secure.")
+st.markdown("- Because the current encryption schemes in place around the globe were built to withstand attacks from *classical* computers, but the rise of *quantum* computers puts the security of these cryptosystems at severe risk of being broken."
+"- The ML-KEM was designed specifically with a post-quantum world in mind. It will soon be used by government agencies and private companies alike to keep information secure.")
 st.markdown("**What is it?**")
 
 
