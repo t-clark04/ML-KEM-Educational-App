fd1d70457494a271db1ed6490b19aee94808d167
t-clark04 <tclark23+UND@nd.edu>
2026-03-17 20:36:31 -0400

    Update Educational.py

--- Educational.py
+++ Educational.py
@@ -2567,5 +2567,5 @@ with tab3:
     st.image(
         "images/Closest Vector Problem.png",
         caption = "Simplified illustration of module learning with errors as a closest vector problem.",
-        width = 300
+        width = 2000
     )
